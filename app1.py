72d7780efd308bb1ded57d83a3d93102e19e68ec
coder-shivam <innocentboy9472@gmail.com>
2025-03-26 16:28:40 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+from dash import Input,Output
+
+
+df = sns.load_dataset('tip')
+app = dash.Dash(__name__)
+server=app.server
+app.layout = html.Div([html.H1 ('Tips Dataset Analysis'),dcc.RadioItems(
+    id = 'x-axis',
+    option = [
+        {'label':'Sex','value':'sex'},
+        {'label':'Smoker','value':'smoker'},
+        {'label':'Day','value':'day'},
+        {'label':'Time','value':'time'}]),dcc.Graph(id='graph')])
+@app.callback(Output('graph','figure'),[Input('axis','value')])
+def update_graph(x_axis):
+    if x_axis =='sex':
+        fig=px.box(df,x='sex',y='total_bill')
+    elif x_axis == 'smoker':
+        fig=px.box(df,x='smoker',y='total_bill')
+    elif x_axis =='day':
+        fig=px.box(df,x='day',y='total_bill')
+    elif x_axis=="time":
+        fig=px.box(df,x='time',y='total_bill')
+    else:
+        return fig
+app.run(debug = True, port = 8050,host='0.0.0.0')
+
